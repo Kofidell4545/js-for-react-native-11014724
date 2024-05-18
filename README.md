3c29e6af37b5f3f7cbfe706591134e45e01cbe3c
Kofidell4545 <davidnhyiraba8@gmail.com>
2024-05-18 20:05:49 +0000

    added task two

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # TASK ONE (processArray)
 
 This involves writing a function that takes array of numbers as input and returns a new arry with the numbers processed according to rules. Even numbers are squared while odd number are multiplied by three or say trippled.
+
+# Task Two ( formattedArrayString)
+
+This function takes two arguments: an array of strings and an array of processed numbers (returned by the processArray function). It returns a new array of strings where the case of each string is determined by the corresponding number in the workedEvenOddnumbers array. If the number is even, the string is converted to uppercase; if the number is odd, the string is converted to lowercase.
--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,5 +1,4 @@
-// Task One
-
+//Task One
 function processArray(numbers) {
   return numbers.map((num) => (num % 2 === 0 ? num ** 2 : num * 3));
 }
@@ -7,3 +6,16 @@ function processArray(numbers) {
 const numbers = [17, 2, 3, 4, 5, 1, 7];
 const workedEvenOddnumbers = processArray(numbers);
 console.log(workedEvenOddnumbers);
+
+// Task Two
+function formatArrayStrings(strings, workedEvenOddnumbers) {
+  return strings.map((string, index) =>
+    workedEvenOddnumbers[index] % 2 === 0
+      ? string.toUpperCase()
+      : string.toLowerCase()
+  );
+}
+
+const strings = ["kofi", "ama", "efia", "maame", "kojo", "kwame", ""];
+const formattedString = formatArrayStrings(strings, workedEvenOddnumbers);
+console.log(formattedString);
